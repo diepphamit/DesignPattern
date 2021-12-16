b74f34a171db122d26cfeb5a00053e9754e6e326
Kevin Pham <phamdinhdiep1998@gmail.com>
2021-12-16 11:14:12 +0700

    added singleton design pattern

---
+++ SingletonPattern/SingletonClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    internal class SingletonClass
+    {
+        private static SingletonClass instance;
+        private static readonly object instanceLock = new object();
+        private SingletonClass() { }
+
+        public static SingletonClass GetInstance()
+        {
+            if(instance == null)
+            {
+                lock (instanceLock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new SingletonClass();
+                    }
+                }
+            }
+            return instance;        
+        }
+    }
+}
---
+++ SingletonPattern/readme.txt
@@ -0,0 +1,1 @@
+﻿This is a singleton pattern readme file.
